b39003c2f68ffcac3673cc5a5bec9bdfb6dd8574
Arthur <arthur.rakhmankulov@ehealth.gov.ua>
2025-04-22 23:33:03 +0300

    comm_1

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from health.views import healthcheck
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', healthcheck, name='health-check'),
 ]
